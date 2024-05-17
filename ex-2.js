b165c8473f0ae6e0c55fdb5e4711dfe98d94608c
Surakit <surakitkunkayun@gmail.com>
2024-05-17 14:29:21 +0700

    feat: finished assignment

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,21 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let num = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      num++;
+    }
+  }
+  if (num >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else if (0 <= num < 5) {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
+
+function scoreOver70(score) {
+  return score > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +24,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, scoreOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, scoreOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, scoreOver70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
+
