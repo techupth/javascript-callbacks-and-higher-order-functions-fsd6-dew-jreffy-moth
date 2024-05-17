48c571d916fc853b2e669552663ad18556da1bb3
Moth <kampanee.moth@gmail.com>
2024-05-17 17:38:16 +0700

    fix: ex-1 and ex-2 .js

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  operation(array);
 }
+
+const forArr = (array) => {
+  for (let arr of array) {
+    newEmployeeSalaries.push(arr + 5000);
+  }
+};
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+forEach(employeeSalaries, forArr);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,19 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let countScores = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      countScores++;
+    }
+  }
+  let scoreResult = operation(countScores);
+  return scoreResult;
 }
+
+const checkScores = (score) => {
+  return score >= 5 ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌";
+};
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -13,3 +25,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScores);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScores);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScores);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
